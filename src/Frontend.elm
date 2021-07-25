9a405ea1b0ffcb301e80464e2678085bb4a7987a
Charlon <charles.assus@gmail.com>
2021-07-25 15:20:55 +0200

    Change padding depending on DeviceClass

--- src/Frontend.elm
+++ src/Frontend.elm
@@ -104,105 +104,125 @@ view : Model -> { title : String, body : List (Html.Html FrontendMsg) }
 view model =
     { title = "AI Financial Expert"
     , body =
-        [ layout [ width fill, height fill ]
-            (column
-                [ height shrink
-                , width fill
-                , paddingXY 32 32
-                ]
-                [ column
-                    [ spacingXY 0 32
-                    , height shrink
-                    , width fill
-                    ]
-                    [ el
-                        [ Font.center
-                        , Font.bold
-                        , Font.color (rgba255 136 170 166 1)
-                        , Font.size 36
-                        , height shrink
-                        , width fill
-                        , Region.heading 1
-                        ]
-                      <|
-                        text "Financial Expert AI"
-                    , paragraph
-                        [ Font.center
-                        , Font.bold
-                        , Font.color (rgba255 46 52 54 1)
-                        , Font.size 24
-                        , height shrink
-                        , width fill
-                        , Region.heading 2
-                        ]
-                        [ text <| "Ask me anything (" ++ String.fromInt model.counter ++ "/30)" ]
-                    , Input.text
-                        [ centerY
-                        , centerX
-                        , spacingXY 0 4
-                        , height shrink
-                        , width
-                            (fill
-                                |> maximum 1024
-                                |> minimum 256
-                            )
-                        , paddingXY 8 8
-                        , Border.rounded 2
-                        , Border.color (rgba255 186 189 182 1)
-                        , Border.solid
-                        , Border.widthXY 1 1
-                        , Font.center
-                        ]
-                        { onChange = TextChanged
-                        , text = model.question
-                        , placeholder = Nothing
-                        , label =
-                            Input.labelAbove
-                                [ Font.color (rgba255 46 52 54 1) ]
-                                (text "")
-                        }
-                    , Input.button
-                        [ Background.color (rgba255 52 101 164 1)
-                        , centerY
-                        , centerX
-                        , Font.center
-                        , Font.color (rgba255 255 255 255 1)
-                        , height shrink
-                        , width shrink
-                        , paddingXY 16 8
-                        , Border.rounded 2
-                        , Border.color (rgba255 52 101 164 1)
-                        , Border.solid
-                        , Border.widthXY 1 1
-                        ]
-                        { onPress =
-                            if model.openAIState == Waiting then
-                                Just SubmitQuestion
+        [ layout [ width fill, height fill ] <|
+            case model.device of
+                Nothing ->
+                    none
 
-                            else
-                                Nothing
-                        , label = text "Submit question"
-                        }
-                    , column
+                Just device ->
+                    let
+                        padding =
+                            case device.class of
+                                Phone ->
+                                    paddingXY 32 32
+
+                                Tablet ->
+                                    paddingXY 128 128
+
+                                Desktop ->
+                                    paddingXY 256 256
+
+                                BigDesktop ->
+                                    paddingXY 512 512
+                    in
+                    column
                         [ height shrink
                         , width fill
+                        , padding
                         ]
-                        [ paragraph
-                            [ centerY
-                            , centerX
-                            , spacingXY 0 4
+                        [ column
+                            [ spacingXY 0 32
                             , height shrink
-                            , width
-                                (fill
-                                    |> maximum 1024
-                                    |> minimum 256
-                                )
+                            , width fill
                             ]
-                            [ paragraph [] [ text "Answer: ", viewResponse model.openAIResponse model.openAIState ] ]
+                            [ el
+                                [ Font.center
+                                , Font.bold
+                                , Font.color <| rgba255 136 170 166 1
+                                , Font.size 36
+                                , height shrink
+                                , width fill
+                                , Region.heading 1
+                                ]
+                              <|
+                                text "Financial Expert AI"
+                            , paragraph
+                                [ Font.center
+                                , Font.bold
+                                , Font.color <| rgba255 46 52 54 1
+                                , Font.size 24
+                                , height shrink
+                                , width fill
+                                , Region.heading 2
+                                ]
+                                [ text <| "Ask me anything (" ++ String.fromInt model.counter ++ "/30)" ]
+                            , Input.text
+                                [ centerY
+                                , centerX
+                                , spacingXY 0 4
+                                , height shrink
+                                , width
+                                    (fill
+                                        |> maximum 1024
+                                        |> minimum 256
+                                    )
+                                , paddingXY 8 8
+                                , Border.rounded 2
+                                , Border.color <| rgba255 186 189 182 1
+                                , Border.solid
+                                , Border.widthXY 1 1
+                                , Font.center
+                                ]
+                                { onChange = TextChanged
+                                , text = model.question
+                                , placeholder = Nothing
+                                , label =
+                                    Input.labelAbove
+                                        [ Font.color <| rgba255 46 52 54 1 ]
+                                    <|
+                                        text ""
+                                }
+                            , Input.button
+                                [ Background.color <| rgba255 52 101 164 1
+                                , centerY
+                                , centerX
+                                , Font.center
+                                , Font.color <| rgba255 255 255 255 1
+                                , height shrink
+                                , width shrink
+                                , paddingXY 16 8
+                                , Border.rounded 2
+                                , Border.color <| rgba255 52 101 164 1
+                                , Border.solid
+                                , Border.widthXY 1 1
+                                ]
+                                { onPress =
+                                    if model.openAIState == Waiting then
+                                        Just SubmitQuestion
+
+                                    else
+                                        Nothing
+                                , label = text "Submit question"
+                                }
+                            , column
+                                [ height shrink
+                                , width fill
+                                ]
+                                [ paragraph
+                                    [ centerY
+                                    , centerX
+                                    , spacingXY 0 4
+                                    , height shrink
+                                    , width
+                                        (fill
+                                            |> maximum 1024
+                                            |> minimum 256
+                                        )
+                                    ]
+                                    [ paragraph [] [ text "Answer: ", viewResponse model.openAIResponse model.openAIState ] ]
+                                ]
+                            ]
                         ]
-                    ]
-                ]
-            )
         ]
     }
 
